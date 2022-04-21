bf697cfd9ad53631452020a0a945fc4070e0bed8
Jose Ozuna <joseozuna48@gmail.com>
2022-04-21 15:02:18 -0700

    Retrieves json files with quotes and displays them on the card

--- scripts.js
+++ scripts.js
@@ -6,36 +6,65 @@ const colors=[
     "#6258C7",
     "#7A4B2C",
     "#615886",
-    "#56191F"
+    "#56191F",
+    "#A56233"
   ]
 
+let quotes={};
+let quote,text;
+
+
+function quotesData(updateInfo){
+  let req = new XMLHttpRequest();
+  req.open("GET", "https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json");
+
+  req.onload = function(){  
+    if(req.status==200){
+      quotes = JSON.parse(this.responseText).quotes;
+      updateInfo();
+      
+    }else{
+      console("Error couldn't  retrieve data");
+      
+    }
+  }
+  req.send();
+}
+
+//Function that selects a random color from a given array.
+function generateColor(array){
+  return array[Math.trunc(Math.random()*array.length)];
+}; 
+
+function generateQuote(quotesList){
+  const result = quotesList[Math.trunc(Math.random()*quotesList.length)];
+  quote = result.quote;
+  author = result.author;
+}
+
+
+function updateInfo(){
+  let color = generateColor(colors);
+  
+  color = generateColor(colors);
+  generateQuote(quotes); 
+
+  $("body,button").css("background-color",color);  
+  $("i,#text,#author").css("color",color);   
+
+  $("#author").text(author);
+  $("#text").text(quote);
+}
+
+
 $(document).ready(function(){
-  function generateColor(array){
-    return array[Math.trunc(Math.random()*array.length)];
-  }; 
-  
-  let newColor = generateColor(colors);  
-  
-  $("body,button").css("background-color",newColor);  
-  $("i,#text,#author").css("color",newColor);    
-  
-  
+  //load json data and displays information on screen
+  quotesData(updateInfo);
+  //Selects a new quote and changes background color when button is pressed
   $("#new-quote").click(function(){
-    newColor = generateColor(colors);
-    $("body,button").css("background-color",newColor);  
-    $("i,#text,#author").css("color",newColor);   
-
-
-    $("#author").text(newColor);
-
-    // $.getJSON("https://gist.github.com/54b659e43b1035215cd0ba1d4577ee80.git", function(data){
-    //   console.log(data);
-    // } )
-
-  
+    updateInfo();
   });
-  
 
-  });
+});
   
   
