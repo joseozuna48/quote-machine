9cccd05e4c7a44812b4dda1b3071d4ba1af1a4cc
Jose Ozuna <joseozuna48@gmail.com>
2022-04-20 17:20:30 -0700

    Added transition when colors are changed

--- scripts.js
+++ scripts.js
@@ -1,47 +1,41 @@
 const colors=[
-    "#F71BDA",
     "#F1371F",
     "#5AA082",
     "#C91CB2",
-   "#181A68",
+    "#181A68",
     "#6258C7",
     "#7A4B2C",
     "#615886",
-    "#56191F",
-    "#D0E9AA"
+    "#56191F"
   ]
+
+$(document).ready(function(){
+  function generateColor(array){
+    return array[Math.trunc(Math.random()*array.length)];
+  }; 
+  
+  let newColor = generateColor(colors);  
+  
+  $("body,button").css("background-color",newColor);  
+  $("i,#text,#author").css("color",newColor);    
   
   
-  $(document).ready(function(){
-    function generateColor(array){
-     return array[Math.trunc(Math.random()*array.length)];
-    }; 
-    
-    let newColor = generateColor(colors);  
-    
-    $("body").css("background-color",newColor);    
-    
-    
-    $("i").css("color",newColor);
-    $("button").css("background-color",newColor);
-    $("#text").css("color",newColor);
-    $("#author").css("color",newColor);
-    
-    
-    
-    $("#new-quote").click(function(){
-      newColor = generateColor(colors);
-      $("body").css("background-color",newColor); 
+  $("#new-quote").click(function(){
+    newColor = generateColor(colors);
+    $("body,button").css("background-color",newColor);  
+    $("i,#text,#author").css("color",newColor);   
+
+
+    $("#author").text(newColor);
+
+    // $.getJSON("https://gist.github.com/54b659e43b1035215cd0ba1d4577ee80.git", function(data){
+    //   console.log(data);
+    // } )
+
   
-      
-      $("i").css("color",newColor);
-      $("button").css("background-color",newColor);
-      $("#text").css("color",newColor);
-      $("#author").css("color",newColor);
-    
-    });
-    
+  });
   
+
   });
   
   
